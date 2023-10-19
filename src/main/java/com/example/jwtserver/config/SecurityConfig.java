e88eeb6fb1d9b427dc15e51c33bcbaed977651d9
woo <wbg030281@gmail.com>
2023-10-19 16:18:45 +0900

    jwt filter 등록 테스트 SecurityConfig에 있는 필터가 먼저 실행 되고 FilterConfig에 있는 필터들은 setOrder()에 있는 번호중 가장 낮은 번호먼저 실행 된다.

--- src/main/java/com/example/jwtserver/config/SecurityConfig.java
+++ src/main/java/com/example/jwtserver/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.jwtserver.config;
 
+import com.example.jwtserver.filter.MyFilter1;
+import com.example.jwtserver.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -16,6 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CorsFilter corsFilter;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter3(),BasicAuthenticationFilter.class);//filterConfig보다 빨리 실행됨
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션 사용안함
                 .and()
